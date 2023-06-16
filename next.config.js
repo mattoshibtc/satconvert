1b79efa54850ee4ff9aab40ecaa4f90ca8b12b12
Mattoshi <mattoshi1@proton.me>
2023-06-16 13:13:28 -0400

    pwa

--- next.config.js
+++ next.config.js
@@ -4,11 +4,14 @@ const repo = 'satconvert'
 const assetPrefix = `/${repo}/`
 const basePath = `/${repo}`
 
+const withPWA = require('next-pwa')({
+  dest: 'public'
+});
 
-const nextConfig = {
+const nextConfig = withPWA({
   output: 'export',
   assetPrefix: assetPrefix,
   basePath: basePath
-}
+})
 
 module.exports = nextConfig
