f066abb8df86e98912e16335682bea6cd4b0f802
Mattoshi <mattoshi1@proton.me>
2023-06-18 19:45:54 -0400

    delete package.lock

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,41 +1,26 @@
 'use client'
 
-import { useState, useEffect, useRef } from 'react'
-import TextField from '@mui/material/TextField'
-import { useNumericFormat } from 'react-number-format';
+import { useState, useEffect } from 'react'
+import CircularProgress from '@mui/material/CircularProgress'
+import Textfield from '@mui/material/TextField'
+import { useNumericFormat } from 'react-number-format'
 
-import { ThemeProvider, createTheme } from '@mui/material/styles';
-import CssBaseline from '@mui/material/CssBaseline';
+import { ThemeProvider, createTheme } from '@mui/material/styles'
+import CssBaseline from '@mui/material/CssBaseline'
+
+const darkTheme = createTheme({
+  palette: {
+    mode: 'dark',
+  },
+});
 
 export default function Home() {
+
   const [isLoading, setIsLoading] = useState(true)
-  const priceFormat = useNumericFormat({
-    thousandSeparator: ",",
-    decimalScale: 0,
-    prefix: '$',
-  })
-
-  const fiatFormat = useNumericFormat({
-    thousandSeparator: ",",
-    decimalScale: 2,
-    prefix: '$',
-  })
-
-  const satsFormat = useNumericFormat({
-    thousandSeparator: ",",
-    decimalScale: 0
-  })
-
   const [fiatPerBtc, setFiatPerBtc] = useState(0)
+  const [fiat, setFiat] = useState("$")
   const [sats, setSats] = useState("")
-  const [fiat, setFiat] = useState("")
-
-  const darkTheme = createTheme({
-    palette: {
-      mode: 'dark',
-    },
-  });
-
+  
   useEffect( () => {
     async function fetchData() {
       const response = await fetch("https://blockchain.info/ticker")
@@ -51,74 +36,120 @@ export default function Home() {
     fetchData()
   }, [])
 
+  const priceFormat = useNumericFormat({
+    thousandSeparator: ",",
+    decimalScale: 0,
+    prefix: '$',
+  })
+  
+  const fiatFormat = useNumericFormat({
+    thousandSeparator: ",",
+    decimalScale: 2,
+    prefix: '$',
+  })
+  
+  const satsFormat = useNumericFormat({
+    thousandSeparator: ",",
+    decimalScale: 0,
+  })
+
   const handleFiatChange = (event: any) => {
     const v = event.target.value
+
+    if (v === "" || v === "$") {
+      setFiat("$")
+      setSats("")
+      return
+    }
+
+    if (v === "$.") {
+      setFiat("$0.")
+      setSats("0")
+      return
+    }
+
     const newFiat = Number(fiatFormat.removeFormatting?.(v))
+    console.log(`v: ${v}`)
+    console.log(`newFiat: ${newFiat}`)
     const newSats = Math.round(100_000_000 / fiatPerBtc * newFiat)
     console.log(`newSats: ${newSats}, newFiat: ${newFiat}`)
-    setFiat(String(fiatFormat.format?.(newFiat.toString())))
+
+    let reformat = String(fiatFormat.format?.(newFiat.toString()))
+
+    // if ends with ".", then add back the decimal point
+    if (v.endsWith(".")) {
+      reformat = reformat + "."
+    } // if ends with  ".0", then add back the ".0"
+    else if (v.endsWith(".0")) {
+      reformat = reformat + ".0"
+    } // if ends with ".00", then add back the ".00"
+    else if (v.endsWith(".00") || v.endsWith(".000")) {
+      reformat = reformat + ".00"
+    }
+
+    setFiat(reformat)
     setSats(String(satsFormat.format?.(newSats.toString())))
   }
 
   const handleSatsChange = (event: any) => {
-    console.log(`values: ${JSON.stringify(event.target.value)}`)
     const newSats = Number(satsFormat.removeFormatting?.(event.target.value))
     const newFiat = fiatPerBtc * newSats / 100_000_000
-    console.log(`newSats: ${newSats}, newFiat: ${newFiat}`)
     setFiat(String(fiatFormat.format?.(newFiat.toString())))
     setSats(String(satsFormat.format?.(newSats.toString())))
   }
 
-  if (isLoading) {
-    return (
-      <main className="flex min-h-screen flex-col items-center">
-        <p>Loading</p>
-      </main>
-    )
-  }
-
   const handleFocus = (event: any) => event.target.select()
+
+  const handleRefresh = () => window.location.reload()
 
   const handleFocusWithSymbol = (event: any) => {
     const t = event.target
     t.setSelectionRange(1, t.value.length)
   }
 
+  let priceVal = <span className="inline-flex">{priceFormat.format?.(fiatPerBtc.toString())} <p className="ml-5 text-[#90caf9]" onClick={handleRefresh}>{'\u27F3'}</p></span>
+
+  if (isLoading) {
+    priceVal = <CircularProgress size={"1.5rem"}/>  
+  }
+
   return (
     <ThemeProvider theme={darkTheme}>
       <CssBaseline />
       <main className="bg-black flex min-h-screen flex-col items-center px-5">
-      <div className="w-9/10">
-        <h1 className="mt-10 mb-10">
-          BTC/USD: { priceFormat.format?.(fiatPerBtc.toString()) }
-        </h1>
-        <div className="mb-5">
-          <TextField
-            label="Dollars" 
-            variant="standard" 
-            inputProps={{
-              style: { fontSize: '4rem'},
-              inputMode: 'numeric'
-            }}
-            value={fiat}
-            onFocus={handleFocusWithSymbol}
-            onChange={handleFiatChange} />
+        <div className="w-9/10">
+          <div className="inline-flex">
+            <div className="mt-10 mb-10 text-white text-2xl">
+              <p>BTC/USD: {priceVal}</p>
+            </div>
+          </div>
+          <div className="mb-5">
+            <Textfield
+              label="Dollars" 
+              variant="standard" 
+              inputProps={{
+                style: { fontSize: '4rem'},
+                inputMode: 'numeric'
+              }}
+              value={fiat}
+              onFocus={handleFocusWithSymbol}
+              onChange={handleFiatChange} />
+          </div>
+          <div className="mb-5">
+            <Textfield 
+              label="Sats" 
+              variant="standard" 
+              inputProps={{
+                style: { fontSize: '4rem'},
+                inputMode: 'numeric'
+              }}
+              value={sats}
+              onFocus={handleFocus}
+              onChange={handleSatsChange} 
+            />
+          </div>
         </div>
-        <div className="mb-5">
-          <TextField 
-            label="Sats" 
-            variant="standard" 
-            inputProps={{
-              style: { fontSize: '4rem'},
-              inputMode: 'numeric'
-            }}
-            value={sats}
-            onFocus={handleFocus}
-            onChange={handleSatsChange} 
-          />
-        </div>
-      </div>
-    </main>
+      </main>
     </ThemeProvider>
   )
 }
